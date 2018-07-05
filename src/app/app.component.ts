9052086fb681169a2fc14f1c2209e95f72531925
Akash Verma <akash.verma926@gmail.com>
2018-07-05 19:15:51 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  games = [
+    {
+      "id":"1",
+      "name": "DOTA 2",
+      "genre": "Strategy"
+    },
+    {
+      "id":"2",
+      "name": "AOE 3",
+      "genre": "Strategy"
+    },
+    {
+      "id":"3",
+      "name": "GTA 5",
+      "genre": "RPG"
+    },
+    {
+      "id":"4",
+      "name": "Far Cry 3",
+      "genre": "Action"
+    },
+    {
+      "id":"5",
+      "name": "GTA San Andreas",
+      "genre": "RPG"
+    },
+    {
+      "id":"6",
+      "name": "Hitman",
+      "genre": "Action"
+    },
+    {
+      "id":"7",
+      "name": "NFS MW",
+      "genre": "Sport"
+    },{
+      "id":"8",
+      "name": "Fifa 16",
+      "genre": "Sport"
+    },{
+      "id":"9",
+      "name": "NFS Sen 2",
+      "genre": "Sport"
+    },{
+      "id":"10",
+      "name": "Witcher Assassins on King",
+      "genre": "Adventure"
+    }
+  ];
+
+  //sorting
+  key: string = 'name';
+  reverse: boolean = false;
+  sortbyname(key){
+    this.key = key;
+  }
+  sortbygenre(key){
+    this.key = key;
+  }
+  sortbynamedesc(key){
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+  sortbygenredesc(key){
+    this.key = key;
+    this.reverse = !this.reverse;
+  }
+  p: number = 1;
+}
